ad5e024833824ad72c0465a5e5636cf07219b9a7
socialdecoders <81747952+socialdecoders@users.noreply.github.com>
2021-04-03 14:09:09 +0100

    Add files via upload

---
+++ images/xmlrpc0db0.php
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?><rsd version="1.0" xmlns="http://archipelago.phrasewise.com/rsd">
+	<service>
+		<engineName>WordPress</engineName>
+		<engineLink>https://wordpress.org/</engineLink>
+		<homePageLink>https://megastoreonline.store</homePageLink>
+		<apis>
+			<api name="WordPress" blogID="1" preferred="true" apiLink="https://megastoreonline.store/xmlrpc.php" />
+			<api name="Movable Type" blogID="1" preferred="false" apiLink="https://megastoreonline.store/xmlrpc.php" />
+			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://megastoreonline.store/xmlrpc.php" />
+			<api name="Blogger" blogID="1" preferred="false" apiLink="https://megastoreonline.store/xmlrpc.php" />
+				<api name="WP-API" blogID="1" preferred="false" apiLink="https://megastoreonline.store/index.php?rest_route=/" />
+			</apis>
+	</service>
+</rsd>
+	
